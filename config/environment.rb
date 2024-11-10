fa2c243e942d46d9fd4898ebcabc6b69be88ea5d
Alonsorozco <Sfabian.orozco@gmail.com>
2024-11-09 22:55:24 -0300

    eliminacion docker

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the Rails application.
 require_relative "application"
 
+config.assets.compile = false
+config.assets.digest = true
+config.assets.precompile += %w( application.js application.css )
+
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
