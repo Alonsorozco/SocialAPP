91c7b7f48c84a24269f6c4cdd0c084c5842b0b75
Alonsorozco <Sfabian.orozco@gmail.com>
2024-11-10 00:38:05 -0300

    puma

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,6 @@
 # Load the Rails application.
 require_relative "application"
 
-config.assets.compile = false
-config.assets.digest = true
-config.assets.precompile += %w( application.js application.css )
-
 
 # Initialize the Rails application.
 Rails.application.initialize!
